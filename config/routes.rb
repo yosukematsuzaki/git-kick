71ff19dd82aa0f20d5050165c38984ab8431616f
EC2 Default User <ec2-user@ip-172-31-35-217.ap-southeast-1.compute.internal>
2018-11-21 08:49:08 +0000

    もっと見るリンクのroutingを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  
   root 'posts#index'
+  resources :posts do
+    member do
+      get :fighter_index
+    end
+  end
   
 end
