b40c21cbedf0c17ef6ec3394d59c0d77bfe7e3d4
EC2 Default User <ec2-user@ip-172-31-35-217.ap-southeast-1.compute.internal>
2018-11-22 09:31:07 +0000

    tagmapsにreferrenceを追加するmigrationファイルを削除

--- db/migrate/20181122084655_add_refences_to_tagmaps.rb
+++
@@ -1,6 +0,0 @@
-class AddRefencesToTagmaps < ActiveRecord::Migration
-  def change
-    add_reference :tagmaps, :post, index: true, foreign_key: true
-    add_reference :tagmaps, :tag, index: true, foreign_key: true
-  end
-end
