c17d67b36c6587f20e3e93755d6c17c083391369
EC2 Default User <ec2-user@ip-172-31-35-217.ap-southeast-1.compute.internal>
2018-11-22 09:43:41 +0000

    admin/posts.rbを修正し、postの管理画面からタグを選択できるようにした

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -11,5 +11,20 @@ ActiveAdmin.register Post do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-  permit_params :url, :fighter_id
+  permit_params :url, :fighter_id, tagmaps_attributes: [:tag_id, :_destroy, :id]
+  
+  form do |f|
+    f.inputs 'Post登録' do
+      f.input :url
+      f.input :fighter_id
+      f.has_many :tagmaps, allow_destroy: true, heading: false,
+                                new_record: true do |ab|
+        ab.input :tag_id,
+                 label: 'タグ',
+                 as: :check_boxes,
+                 collection: Tag.all.map { |a| [a.name, a.id] }
+      end
+      f.actions
+    end
+  end
 end
