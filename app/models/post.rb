3c67f286917b51c1216dedc740bc518a3091b04a
EC2 Default User <ec2-user@ip-172-31-35-217.ap-southeast-1.compute.internal>
2018-11-22 09:07:46 +0000

    post.rbにaccepts_nested_attributes_forを追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :fighter
   has_many :tags, through: :tagmaps
   has_many :tagmaps, dependent: :destroy
+  
+  accepts_nested_attributes_for :tagmaps, allow_destroy: true
 end
