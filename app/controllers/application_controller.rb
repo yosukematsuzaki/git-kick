7ddcc18955e02b204d4d47a155a504da6098177c
EC2 Default User <ec2-user@ip-172-31-35-217.ap-southeast-1.compute.internal>
2018-11-26 09:14:55 +0000

    categoryのdropdownで、データベースからfighter名を取得できるように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :get_info
+  
+  def get_info
+    @fighters = Fighter.all
+    @tags = Tag.all
+  end
 end
