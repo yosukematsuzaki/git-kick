759cc16134503c7e8ac2739f4d3160e90e223c8e
EC2 Default User <ec2-user@ip-172-31-35-217.ap-southeast-1.compute.internal>
2018-12-05 12:33:17 +0000

    indexページにレスポンシブを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  before_filter :get_info
+  before_action :get_info
   
+  private
   def get_info
     @get_fighters = Fighter.all
     @get_tags = Tag.all
   end
+
 end
