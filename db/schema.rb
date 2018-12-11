3d32e356be00bb73429d0ede90d657a8c76d3f9f
EC2 Default User <ec2-user@ip-172-31-35-217.ap-southeast-1.compute.internal>
2018-12-11 09:21:42 +0000

    tagsテーブルにparent_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181122093525) do
+ActiveRecord::Schema.define(version: 20181211091904) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20181122093525) do
     t.text     "name",       limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "parent_id",  limit: 4
   end
 
   add_foreign_key "tagmaps", "posts"
